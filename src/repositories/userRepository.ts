1130077e0621804702fb9c00d65309df45f3d629
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-10-14 18:58:39 +0530

    implimented userBaseRepository and userRepository

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,68 +1,57 @@
 import userModel, { UserInterface } from "../models/userModel";
-import { comRepository } from "./comRepository";
+import { UserBaseRepository } from "./BaseRepository/UserBaseRepository";
+import { Document } from "mongoose";
 
-class UserRepository implements comRepository<UserInterface> {
-  async saveUser(
-    userData: Partial<UserInterface>
-  ): Promise<UserInterface | null> {
-    try {
-      const newUser = new userModel(userData);
-      await newUser.save();
-      return newUser as UserInterface;
-    } catch (error) {
-      console.log("Error from userRepository", error as Error);
-      throw error
-    }
+class UserRepository extends UserBaseRepository<UserInterface & Document> {
+  constructor() {
+    super(userModel); // Pass the user model to the BaseRepository
   }
 
+  // Method from BaseRepository: saveUser (this is now handled by BaseRepository save)
+  async saveUser(userData: Partial<UserInterface>): Promise<UserInterface | null> {
+    return this.save(userData); // Use the base class method
+  }
+
+  // Custom method to find user by email
   async findEmail(email: string): Promise<UserInterface | null> {
     try {
-      const userFound = await userModel.findOne({ email: email });
-      console.log("user is ", userFound);
+      const userFound = await this.findOne({ email });
       if (userFound) {
         console.log("user email found successfully", userFound);
-        return userFound as UserInterface;
+        return userFound;
       }
       return null;
     } catch (error) {
-      console.log(
-        "error found in the userRepository while finding the email ",
-        error as Error
-      );
-      throw error
-    
-    }
-  }
-
-  async emailExistCheck(email: string): Promise<UserInterface | null> {
-    try {
-      const userFound = await userModel.findOne({ email: email });
-      return userFound as UserInterface;
-    } catch (error) {
-      console.log(error as Error);
-      throw error
-    }
-  }
-
-  async updateNewPassword(password: string, userId: string) {
-    try {
-      const user = await userModel.findById(userId);
-      if (user) user.password = password;
-      const updatedUser = await user?.save();
-      return updatedUser;
-    } catch (error) {
-      console.log(error as Error);
+      console.log("Error in UserRepository while finding the email", error as Error);
       throw error;
     }
   }
 
-  async getUserById(id: string): Promise<UserInterface | null> {
+  // Method to check if an email exists (this is just another alias for findEmail)
+  async emailExistCheck(email: string): Promise<UserInterface | null> {
+    console.log("email find in userRepsoi",email);
+
+    return this.findOne({ email });
+  }
+
+  // Custom method to update the user's password
+  async updateNewPassword(password: string, userId: string): Promise<UserInterface | null> {
     try {
-      return await userModel.findById(id);
+      const user = await this.findById(userId);
+      if (user) {
+        user.password = password;
+        return await user.save();
+      }
+      return null;
     } catch (error) {
-      console.log(error as Error);
-      throw error
+      console.log("Error in UserRepository while updating password", error as Error);
+      throw error;
     }
+  }
+
+  // Method from BaseRepository: getUserById (findById is already handled by BaseRepository)
+  async getUserById(id: string): Promise<UserInterface | null> {
+    return this.findById(id); // Use the base class method
   }
 }
 
