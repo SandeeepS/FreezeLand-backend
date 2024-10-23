451ee7c4e1d9040e946776efe3849141baa4e5a7
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-10-23 08:27:26 +0530

    added address to the userModel

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,14 +1,33 @@
 import mongoose, { Schema, Document, Model } from "mongoose";
 
+const AddressSchema = new Schema({
+  name: { type: String, require: true },
+  phone: { type: Number, require: true },
+  email: { type: String, require: true },
+  state: { type: String, require: true },
+  pin: { type: Number, require: true },
+  district: { type: String, require: true },
+  landMark: { type: String, require: true },
+});
+
 export interface UserInterface extends Document {
   _id: mongoose.Types.ObjectId;
   name: string;
   password: string;
   email: string;
   phone: number;
+  profile_picture: string;
+  address: {
+    name: string;
+    phone: number;
+    email: string;
+    state: string;
+    pin: string;
+    district: string;
+    landMark: string;
+  }[];
   isBlocked: boolean;
   isDeleted: boolean;
-  profile_picture: string;
 }
 
 const userSchema: Schema<UserInterface> = new Schema({
@@ -30,6 +49,10 @@ const userSchema: Schema<UserInterface> = new Schema({
     type: String,
     default:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/680px-Default_pfp.svg.png",
+  },
+  address: {
+    type: [AddressSchema],
+    required: false,
   },
   isBlocked: {
     type: Boolean,
