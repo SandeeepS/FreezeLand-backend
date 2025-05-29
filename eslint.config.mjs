55513381472c4688cc458277c9142e4859eafdaa
SandeeepS <sandeepsanthosh3303@gmail.com>
2025-05-29 21:06:05 +0530

    created github folder , workflows , and test.yml file . and modified the package.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,9 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {languageOptions: { globals: globals.browser}},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
