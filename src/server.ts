73d5330e10cf57f90375e34e05e8411d70bdbb91
SandeeepS <sandeepsanthosh3303@gmail.com>
2025-06-13 16:04:59 +0530

    changed the server.ts file

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+
 import express, { Express, Request, Response } from "express";
 import http from "http";
 import errorHandlerMiddleware from "./middlewares/errorHandler";
 import cors from "cors";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes";
 import adminRoutes from "./routes/adminRoutes";
 import mechRoutes from "./routes/mechRoutes";
@@ -13,7 +16,6 @@ import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import configureSocket from "./config.ts/socket";
 
-dotenv.config();
 const morganFormat = ":method :url :status :response-time ms";
 const app: Express = express();
 const PORT: string | number = process.env.PORT || 5000;
@@ -21,9 +23,18 @@ const PORT: string | number = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
+// app.use(
+//   cors({
+//     origin: ["https://freezeland.online","http://localhost:5173"],
+//     credentials: true,
+//   })
+// );
+
 app.use(
   cors({
-    origin: ["https://freezeland.online","http://localhost:5173"],
+    origin: function (origin, callback) {
+      callback(null, origin || "*"); // Reflects the request origin
+    },
     credentials: true,
   })
 );
