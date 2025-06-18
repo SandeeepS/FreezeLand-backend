5ac33f5b80e6e4e6b534c6bbe57424a6db04b6b5
SandeeepS <sandeepsanthosh3303@gmail.com>
2025-06-18 16:35:21 +0530

    removed the commented part in the server.ts

--- src/server.ts
+++ src/server.ts
@@ -23,12 +23,7 @@ const PORT: string | number = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
-// app.use(
-//   cors({
-//     origin: ["https://freezeland.online","http://localhost:5173"],
-//     credentials: true,
-//   })
-// );
+
 
 app.use(
   cors({
