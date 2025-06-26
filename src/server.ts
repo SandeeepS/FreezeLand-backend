6fe89369c28513613c41878516dead33d8f34e2a
SandeeepS <sandeepsanthosh3303@gmail.com>
2025-06-26 17:53:23 +0530

    changes made in the server.ts file

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-
 import express, { Express, Request, Response } from "express";
 import http from "http";
 import errorHandlerMiddleware from "./middlewares/errorHandler";
@@ -15,15 +14,10 @@ import logger from "./utils/logger";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import configureSocket from "./config.ts/socket";
-
 const morganFormat = ":method :url :status :response-time ms";
 const app: Express = express();
 const PORT: string | number = process.env.PORT || 5000;
-
-
 const server = http.createServer(app);
-
-
 
 app.use(
   cors({
